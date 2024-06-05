5783c7f1a2b303991b29c1ab024a81f9fa860597
Temuulen Narmandakh <temuulen.n@pinecone.mn>
2024-06-05 11:31:42 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,114 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
+
+type Data = {
+  id: number;
+  name: string;
+};
 
 export default function Home() {
+  const [data] = useState<Data[]>([
+    { id: 1, name: "Hello" },
+    { id: 2, name: "Bye" },
+  ]);
+  const [selectedData, setSelectedData] = useState<Data[]>([]);
+  const [submittingData, setSubmittingData] = useState<Data[]>([]);
+  const [submittedData, setSubmittedData] = useState<Data[]>([]);
+
+  const onCheck = (id: number) => {
+    const triggeringData = data.find((row) => row.id === id);
+    if (selectedData.includes(triggeringData!)) {
+      setSelectedData([...selectedData].filter((row) => row.id !== id));
+    } else {
+      setSelectedData([...selectedData, triggeringData!]);
+    }
+  };
+
+  const onBatchSave = () => {
+    setSubmittingData(selectedData);
+    setSelectedData([]);
+  };
+
+  const onSubmit = (id: number) => {
+    const triggeringData = data.find((row) => row.id === id);
+    if (selectedData.includes(triggeringData!)) {
+      setSubmittingData([...selectedData].filter((row) => row.id !== id));
+    } else {
+      setSubmittingData([...selectedData, triggeringData!]);
+    }
+    setSubmittedData([...submittedData, triggeringData!]);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className="flex w-full h-screen items-center justify-center flex-col gap-2">
+      <ul className="gap-2 flex flex-col">
+        {data.map((row) => {
+          const checked = selectedData.filter((selectedItem) => selectedItem.id === row.id).length > 0;
+          const isSubmitting = submittingData.filter((submittingItem) => submittingItem.id === row.id).length > 0;
+          const isSubmitted = submittedData.filter((submittedItem) => submittedItem.id === row.id).length > 0;
+          return (
+            <ListItem onSubmit={onSubmit} isSubmitting={!isSubmitted && isSubmitting} onCheck={onCheck} checked={checked} key={row.id} data={row} />
+          );
+        })}
+      </ul>
+      <button className="btn" onClick={onBatchSave}>
+        batch save
+      </button>
+    </div>
   );
 }
+
+const ListItem = ({
+  data,
+  checked,
+  onCheck,
+  isSubmitting,
+  onSubmit,
+}: {
+  data: Data;
+  checked: boolean;
+  onCheck: (id: number) => void;
+  isSubmitting: boolean;
+  onSubmit: (id: number) => void;
+}) => {
+  const [submitted, setSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const load = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setSubmitted(true);
+    }, 1000);
+  };
+  useEffect(() => {
+    if (isSubmitting) {
+      load();
+      onSubmit(data.id);
+    }
+  }, [isSubmitting, onSubmit]);
+  return (
+    <li>
+      <label className="flex gap-4 items-center">
+        <input
+          type="checkbox"
+          checked={submitted || checked}
+          disabled={submitted}
+          className="checkbox"
+          onChange={() => {
+            onCheck(data.id);
+          }}
+        />
+        {data.name}
+        <button className="btn" onClick={load} disabled={submitted}>
+          {loading ? (
+            <>
+              <span className="loading loading-ring loading-xs"></span>
+            </>
+          ) : (
+            <>{submitted ? <>done</> : <>go</>}</>
+          )}
+        </button>
+      </label>
+    </li>
+  );
+};
